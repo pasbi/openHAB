6222e68b98fe03bcb81c4d58e66c6b5225de6c69
Pascal Bies <pascal.bies@k-lens.de>
2021-01-03 16:24:17 +0100

    install vim plugin

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ sudo sh -c "echo 'deb http://phoscon.de/apt/deconz $(lsb_release -cs) main' > /e
 sudo apt update -y
 sudo apt install deconz -y
 sudo systemctl enable deconz
+
+mkdir -p ~/.vim/{ftdetect,syntax}
+curl -L -o ~/.vim/syntax/openhab.vim https://github.com/cyberkov/openhab-vim/raw/master/syntax/openhab.vim
+curl -L -o ~/.vim/ftdetect/openhab.vim https://github.com/cyberkov/openhab-vim/raw/master/ftdetect/openhab.vim
